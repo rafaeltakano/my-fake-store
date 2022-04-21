7cdfa75d10f10173c307cab4330187913010353a
rafael takano <rafael-takano@hotmail.com>
2022-04-21 05:32:20 -0300

    refactor(route/home): consumindo e tratando api ao abrir a home page

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,3 +1,55 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+const isloading = ref(false)
+
+const apiData = ref([])
+
+const categories = async () => {
+  const { data } = await axios.get('https://fakestoreapi.com/products/categories')
+  return data
+}
+
+const products = async () => {
+  const { data } = await axios.get('https://fakestoreapi.com/products')
+  return data
+}
+
+const handleApi = async (categories, products) => {
+  let treatedApi = new Object()
+
+  categories.filter((item) => {
+    treatedApi[item] = new Array()
+  })
+
+  Object.values(products).filter((item) => {
+    treatedApi[item.category].push(item)
+  })
+
+  return treatedApi
+}
+
+onMounted(async () => {
+  isloading.value = true
+
+  const [cat, prod] = await Promise.all([categories(), products()])
+
+  apiData.value = await handleApi(cat, prod)
+
+  isloading.value = false
+
+  return {
+    isloading,
+    apiData,
+  }
+})
+</script>
 <template>
-    <h1>Lorem ipsum dolor sit amet.</h1>
+  <AppLayout title="Home" :isloading="isloading">
+    <template #content>
+      <div class="test-container">lorem</div>
+    </template>
+  </AppLayout>
 </template>
+
