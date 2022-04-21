f5276e6574426c31de9bc417ff30d20fa8571541
rafael takano <rafael-takano@hotmail.com>
2022-04-21 03:45:53 -0300

    feat: criado componente NavBarCard para exibir cartoes dos produtos na nav por categorias

---
+++ resources/js/Jetstream/NavBarCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  category: String,
+  description: String,
+  id: Number,
+  image: String,
+  price: Number,
+  rating: Object,
+  title: String,
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 items-center">
+    <img :src="image" :alt="title" class="object-contain h-44 w-80" />
+    <p class="font-bold">{{ title }}</p>
+    <p>Shop Now</p>
+  </div>
+</template>
+
