61f12cd6544973cbc2538092bf4505d9d4109837
rafael takano <rafael-takano@hotmail.com>
2022-04-24 02:14:58 -0300

    refactor: adicionado carrinho para persistir os dados do carrinho

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -8,58 +8,62 @@
       </div>
     </div>
 
-    <div class="flex flex-col" v-else>
-      <NavBar>
-        <template #nav-mobile>
-          <NavBarMobile :apiData="apiData" :open="open" @close="open = false" />
-        </template>
+    <div class="viewport" v-else>
+      <div class="viewport__navbar flex flex-col">
+        <NavBar>
+          <template #nav-mobile>
+            <NavBarMobile :apiData="apiData" :open="open" @close="open = false" />
+          </template>
 
-        <template #nav-mobile-sandwich>
-          <button
-            type="button"
-            class="bg-white p-2 rounded-md text-gray-400 lg:hidden"
-            @click="open = true"
-          >
-            <span class="sr-only">Open menu</span>
-            <MenuIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </template>
+          <template #nav-mobile-sandwich>
+            <button
+              type="button"
+              class="bg-white p-2 rounded-md text-gray-400 lg:hidden"
+              @click="open = true"
+            >
+              <span class="sr-only">Open menu</span>
+              <MenuIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </template>
 
-        <template #nav-popover>
-          <NavBarPopover :apiData="apiData" :open="open" @add-item="addToCart" />
-        </template>
+          <template #nav-popover>
+            <NavBarPopover :apiData="apiData" :open="open" @add-item="addToCart" />
+          </template>
 
-        <template #nav-search-input>
-          <NavBarSearch :options="options" />
-        </template>
+          <template #nav-search-input>
+            <NavBarSearch :options="options" />
+          </template>
 
-        <template #nav-shopping-bag>
-          <ShoppingBagIcon
-            class="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
-            aria-hidden="true"
-            @click="cart = true"
-          />
-          <NavBarBag
-            :cartProducts="cartProducts"
-            :open="cart"
-            @close="cart = false"
-            @remove-item="removeFromCart"
-            @checkout="checkout"
-          />
-          <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-            {{ cartTrackNumber }}
-          </span>
-          <span class="sr-only">items in cart, view bag</span>
-        </template>
-      </NavBar>
+          <template #nav-shopping-bag>
+            <ShoppingBagIcon
+              class="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+              aria-hidden="true"
+              @click="cart = true"
+            />
+            <NavBarBag
+              :cartProducts="cartProducts"
+              :open="cart"
+              @close="cart = false"
+              @remove-item="removeFromCart"
+              @checkout="checkout"
+            />
+            <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+              {{ cartTrackNumber }}
+            </span>
+            <span class="sr-only">items in cart, view bag</span>
+          </template>
+        </NavBar>
+      </div>
 
-      <slot name="content" :addProduct="addToCart"> </slot>
+      <div class="viewport__content max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <slot name="content" :addProduct="addToCart"> </slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, toRaw } from 'vue'
 import { Head, Link } from '@inertiajs/inertia-vue3'
 import Alert from '@/Jetstream/Commons/Alert.vue'
 import Spinner from '@/Jetstream/Commons/Spinner.vue'
@@ -104,6 +108,39 @@ const handleApi = async (categories, products) => {
   return treatedApi
 }
 
+const handleCart = () => {
+  if (sessionStorage.getItem('sessionCart')) {
+    cartProducts.value = JSON.parse(sessionStorage.getItem('sessionCart'))
+    cartTrackNumber.value = JSON.parse(sessionStorage.getItem('sessionCartTrackNumber'))
+    return true
+  }
+
+  sessionStorage.setItem('sessionCart', '')
+  sessionStorage.setItem('sessionCartTrackNumber', 0)
+  return false
+}
+
+const pushFromNavToCart = (exists, product, productIdx) => {
+  if (!exists) {
+    product.quantity = 1
+    cartProducts.value.push(product)
+    cartTrackNumber.value++
+  } else {
+    cartProducts.value[productIdx].quantity++
+    cartTrackNumber.value++
+  }
+}
+
+const pushFromProductToCart = (exists, product, productIdx) => {
+  if (!exists) {
+    cartProducts.value.push(product)
+    cartTrackNumber.value += product.quantity
+  } else {
+    cartProducts.value[productIdx].quantity += product.quantity
+    cartTrackNumber.value += product.quantity
+  }
+}
+
 const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
@@ -131,27 +168,13 @@ const addToCart = (item) => {
   })
 
   if (!('quantity' in product)) {
-    //from navbar
-    if (!exists) {
-      //exits in the cart
-      product.quantity = 1
-      cartProducts.value.push(product)
-      cartTrackNumber.value++
-    } else {
-      cartProducts.value[productIdx].quantity++
-      cartTrackNumber.value++
-    }
+    pushFromNavToCart(exists, product, productIdx)
   } else {
-    //from product page
-    if (!exists) {
-      //exits in the cart
-      cartProducts.value.push(product)
-      cartTrackNumber.value += product.quantity
-    } else {
-      cartProducts.value[productIdx].quantity += product.quantity
-      cartTrackNumber.value += product.quantity
-    }
+    pushFromProductToCart(exists, product, productIdx)
   }
+
+  sessionStorage.setItem('sessionCart', JSON.stringify(cartProducts.value))
+  sessionStorage.setItem('sessionCartTrackNumber', JSON.stringify(cartTrackNumber.value))
 
   Toast.fire({
     icon: 'success',
@@ -200,9 +223,15 @@ onMounted(async () => {
 
     options.value = prod
 
+    handleCart()
+
     isloading.value = false
   } catch (exception) {
-    Swal.fire(exception, 'Please, refresh the page, somethimes this happens 😅', 'warning')
+    Swal.fire(
+      'Oops...',
+      'Please, refresh the page, sometimes fakestoreapi blocks localhost and this happens 😅',
+      'info'
+    )
   } finally {
     return {
       isloading,
