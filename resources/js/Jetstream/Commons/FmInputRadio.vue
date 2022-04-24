6ce1cf67fd1cfadf18d4059cd717fdcec0e98be6
rafael takano <rafael-takano@hotmail.com>
2022-04-24 09:06:57 -0300

    feat: criado componente de input radio

---
+++ resources/js/Jetstream/Commons/FmInputRadio.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+import { ElRadio } from 'element-plus'
+
+const props = defineProps({
+  label: [Boolean, Number, String],
+})
+
+const radio = ref('')
+</script>
+
+<template>
+  <el-radio v-model="radio" :label="label" size="large" @change="$emit('input-change')">
+    <slot></slot>
+  </el-radio>
+</template>
+
