d283199faf5209faec3714e3c951dc9ebb6635d9
rafael takano <rafael-takano@hotmail.com>
2022-04-24 02:06:47 -0300

    feat: criado componente para escolher cores do produto

---
+++ resources/js/Jetstream/Commons/ColorPicker.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ElRow, ElButton } from 'element-plus'
+</script>
+
+<template>
+  <el-row>
+    <el-button type="primary" circle />
+    <el-button type="success" circle />
+    <el-button type="info" circle />
+    <el-button type="warning" circle />
+    <el-button type="danger" circle />
+  </el-row>
+</template>
+
+<style scoped>
+::v-deep .el-button.is-circle {
+  padding: 1rem;
+}
+</style>
+
